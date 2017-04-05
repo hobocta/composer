4dd08e18394b45472d8aa463371cf64ca915f7e3
Антон Кондратов <contact@site-home.ru>
2017-04-05 12:40:45 +0300

    refactoring

--- Hbc/Composer/ComposerTest.php
+++ Hbc/Composer/ComposerTest.php
@@ -1,8 +1,15 @@
 <?php
 namespace Hbc\Composer;
 
+/**
+ * Class ComposerTest
+ * @package Hbc\Composer
+ */
 class ComposerTest
 {
+	/**
+	 * @return int
+	 */
 	public function test()
 	{
 		return rand(0, 100);
